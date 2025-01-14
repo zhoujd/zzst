77454517d1bd67d6da31b18141ebce3b23e62f71
Zachary Zhou <zchrzhou@gmail.com>
2025-01-14 10:44:03 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,15 +1,26 @@
 #!/bin/bash
 
 SCRIPT_ROOT=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
+CORE_ROOT=$(cd $SCRIPT_ROOT && pwd)
+CORE_TOP=$(cd $CORE_ROOT/.. && pwd)
 
 install_dep() {
     echo "Install apt pkg"
     sudo apt update
     sudo apt install -y libx11-dev libxft-dev libxinerama-dev
     sudo apt install -y libgd-dev
-    echo "Install bin"
-    sudo cp -v $SCRIPT_ROOT/bin/st-editscreen /usr/local/bin/
     echo "Install dep done"
+}
+
+install_bin() {
+    local target=/usr/local/bin
+    local apps=(
+        $CORE_ROOT/bin/editscreen
+    )
+    for app in ${apps[@]}; do
+        sudo cp -fv $app $target
+    done
+    echo "Install bin done"
 }
 
 build() {
@@ -28,6 +39,7 @@ build() {
 
 all() {
     install_dep
+    install_bin
     build
     echo "Install all done"
 }
@@ -37,6 +49,7 @@ usage() {
     cat <<EOF
 Usage: $app {dep|build|-b|all|-a}
 dep          --    Install build dependence
+bin          --    Install bin
 build|-b     --    Build all
 all|-a       --    Install all
 EOF
@@ -45,6 +58,9 @@ EOF
 case $1 in
     dep )
         install_dep
+        ;;
+    bin )
+        install_bin
         ;;
     build|-b )
         build
