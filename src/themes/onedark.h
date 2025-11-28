353a0f60f01abf5171f2309a9c274f1a8ea5cb29
Zachary Zhou <zchrzhou@gmail.com>
2025-11-28 17:52:23 +0800

    st: update theme files

--- src/themes/onedark.h
+++ src/themes/onedark.h
@@ -2,24 +2,24 @@
 
 static const char *colorname[] = {
 	/* 8 normal colors */
-	[0] = "#1e2127", 				/* black */
-	[1] = "#e06c75",				/* red */
-	[2] = "#98c379",				/* green */
-	[3] = "#d19a66",				/* yellow */
-	[4] = "#61afef",				/* blue */
-	[5] = "#c678dd",				/* magenta */
-	[6] = "#56b6c2",				/* cyan */
-	[7] = "#abb2bf",				/* white */
+	"#1e2127", /* black   */
+	"#e06c75", /* red     */
+	"#98c379", /* green   */
+	"#d19a66", /* yellow  */
+	"#61afef", /* blue    */
+	"#c678dd", /* magenta */
+	"#56b6c2", /* cyan    */
+	"#abb2bf", /* white   */
 
 	/* 8 bright colors */
-	[8] = "#5c6370",				/* black */
-	[9] = "#e06c75",				/* red */
-	[10] = "#98c379",				/* green */
-	[11] = "#d19a66",				/* yellow */
-	[12] = "#61afef",				/* blue */
-	[13] = "#c678dd",				/* magenta */
-	[14] = "#56b6c2",				/* cyan */
-	[15] = "#ffffff",				/* white */
+	"#5c6370", /* black   */
+	"#e06c75", /* red     */
+	"#98c379", /* green   */
+	"#d19a66", /* yellow  */
+	"#61afef", /* blue    */
+	"#c678dd", /* magenta */
+	"#56b6c2", /* cyan    */
+	"#ffffff", /* white   */
 
 	[255] = 0,
 
