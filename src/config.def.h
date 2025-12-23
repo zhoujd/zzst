3618d5cd91e1abbae98bb2560af2d31261700055
Zachary Zhou <zchrzhou@gmail.com>
2025-12-23 23:31:12 +0800

    st: update config.def.h on default theme

--- src/config.def.h
+++ src/config.def.h
@@ -107,7 +107,7 @@ unsigned int tabspaces = 8;
 float alpha = 0.9;
 
 /* Terminal colors (16 first used in escape sequence) */
-#include "themes/nord.h"
+#include "themes/gruvbox.h"
 
 /*
  * Default shape of cursor
