46289762d1036858c405ae8fc949ec59dffdfa06
Zachary Zhou <zchrzhou@gmail.com>
2026-05-28 11:38:19 +0800

    st: clean config.def.h on mouse 4/5 scroll

--- src/config.def.h
+++ src/config.def.h
@@ -199,14 +199,12 @@ static char *pagescreencmd[] = { "pagescreen", "externalpipe", NULL};
  * Beware that overloading Button1 will disable the selection.
  */
 static MouseShortcut mshortcuts[] = {
-	/* mask                 button   function        argument       release */
-	{ XK_ANY_MOD,           Button4, kscrollup,      {.i = 1},      0, /* !alt */ -1 },
-	{ XK_ANY_MOD,           Button5, kscrolldown,    {.i = 1},      0, /* !alt */ -1 },
-	{ XK_ANY_MOD,           Button3, selpaste,       {.i = 0},      1 },
-	{ ShiftMask,            Button4, ttysend,        {.s = "\033[5;2~"} },
-	{ XK_ANY_MOD,           Button4, ttysend,        {.s = "\031"} },
-	{ ShiftMask,            Button5, ttysend,        {.s = "\033[6;2~"} },
-	{ XK_ANY_MOD,           Button5, ttysend,        {.s = "\005"} },
+	/* mask                 button   function        argument    release  alt */
+	{ XK_ANY_MOD,           Button3, selpaste,       {.i = 0},   1 },
+	{ ShiftMask,            Button4, ttysend,        {.s = "\033[5;2~"}, 0, 1 },
+	{ XK_ANY_MOD,           Button4, kscrollup,      {.i = 1},   0,        -1 },
+	{ ShiftMask,            Button5, ttysend,        {.s = "\033[6;2~"}, 0, 1 },
+	{ XK_ANY_MOD,           Button5, kscrolldown,    {.i = 1},   0,        -1 },
 };
 
 /* Internal keyboard shortcuts. */
