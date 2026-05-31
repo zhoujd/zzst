863d6e3b53aa5dc6864728116e6bf2b55060b884
Zachary Zhou <zchrzhou@gmail.com>
2026-05-31 17:00:33 +0800

    st: update nord theme

--- src/themes/nord.h
+++ src/themes/nord.h
@@ -5,24 +5,24 @@
 
 static const char *colorname[] = {
 	/* 8 normal colors */
-	"#3b4252", /* black   */
-	"#bf616a", /* red     */
-	"#a3be8c", /* green   */
-	"#ebcb8b", /* yellow  */
-	"#81a1c1", /* blue    */
-	"#b48ead", /* magenta */
-	"#88c0d0", /* cyan    */
-	"#e5e9f0", /* white   */
+	"#3b4252", /* black   (nord1) */
+	"#bf616a", /* red     (nord11) */
+	"#a3be8c", /* green   (nord14) */
+	"#ebcb8b", /* yellow  (nord13) */
+	"#81a1c1", /* blue    (nord9) */
+	"#b48ead", /* magenta (nord15) */
+	"#88c0d0", /* cyan    (nord8) */
+	"#e5e9f0", /* white   (nord5) */
 
 	/* 8 bright colors */
-	"#4c566a", /* black   */
-	"#bf616a", /* red     */
-	"#a3be8c", /* green   */
-	"#ebcb8b", /* yellow  */
-	"#81a1c1", /* blue    */
-	"#b48ead", /* magenta */
-	"#8fbcbb", /* cyan    */
-	"#eceff4", /* white   */
+	"#4c566a", /* black   (nord3) */
+	"#bf616a", /* red     (nord11) */
+	"#a3be8c", /* green   (nord14) */
+	"#ebcb8b", /* yellow  (nord13) */
+	"#81a1c1", /* blue    (nord9) */
+	"#b48ead", /* magenta (nord15) */
+	"#8fbcbb", /* cyan    (nord7) */
+	"#eceff4", /* white   (nord6) */
 
 	[255] = 0,
 
